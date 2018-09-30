5c6f197924524ccca7902fff1c7f7f01b064f98e
brunoleocoelho <brunoleocoelho@gmail.com>
2018-09-29 22:10:39 -0300

    Correções!

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-<<<<<<< HEAD
 import { ServerConnectService } from '../server-connect.service';
-=======
-import { Project } from '../models/user.model';
 import { UserService } from '../services/user-service';
->>>>>>> 6a7d4d603a9f7c3eec5bc0764537246e6dd22787
+import { Project } from '../models/user.model';
+
 
 @Component({
     selector: 'app-workspace',
@@ -14,24 +12,23 @@ import { UserService } from '../services/user-service';
 })
 export class WorkspaceComponent implements OnInit {
 
-<<<<<<< HEAD
-    incluindo: boolean = false;
     data: any;
-    projetos = [];
-    segmentos = [];
     novoProj: NovoProjeto;
+    projects = new Array<Project>();
+    project = new Project();
 
-    constructor(private rout: Router, private serv: ServerConnectService) {
+    showForm = false;
+
+    constructor(private rout: Router, private serv: ServerConnectService, private userService: UserService) {
     }
 
     ngOnInit() {
-        let loginStrg = window.localStorage.getItem('login-data')
-        if (loginStrg) {
-            if (loginStrg.length == 0 || loginStrg.length == null) {
-                this.rout.navigate(['login']);
-            }
-            this.getProjects();
+        const loginStrg = window.localStorage.getItem('user');
+        if (loginStrg === null || loginStrg.length === 0) {
+            this.rout.navigate(['login']);
         }
+        console.log(loginStrg);
+        this.getProjects();
     }
 
     /** cria um novo projeto para o empreendedor */
@@ -45,30 +42,11 @@ export class WorkspaceComponent implements OnInit {
         this.serv.getProjects().subscribe(
             ok => {
                 this.data = ok;
-                this.projetos = this.data.projetos;
-                console.log("projetos:", this.projetos);
                 // this.segmentos = this.data.segmento;
                 // console.log("segmentos:", this.segmentos);
             },
-            err => { console.log("Erro.getProjects:", err)}
+            err => { console.log("Erro.getProjects:", err) }
         );
-=======
-    projects = new Array<Project>();
-    project = new Project();
-    showForm = false;
-
-    constructor(private rout: Router,
-                private userService: UserService) {
-    }
-
-    ngOnInit() {
-        const loginStrg = window.localStorage.getItem('user');
-        if (loginStrg === null || loginStrg.length === 0) {
-                this.rout.navigate(['login']);
-            }
-        console.log(loginStrg);
-    }
-    loadMyProjectList() {
     }
 
     addMyProject() {
@@ -76,12 +54,15 @@ export class WorkspaceComponent implements OnInit {
         this.project = new Project();
         console.log(this.projects);
         this.showForm = false;
->>>>>>> 6a7d4d603a9f7c3eec5bc0764537246e6dd22787
     }
+
+    // loadMyProjectList() {
+    // }
+
 }
 
 /** Classe que instancia um novo projeto */
-class NovoProjeto{
+class NovoProjeto {
     titulo;
     segmento;
     competencias;
